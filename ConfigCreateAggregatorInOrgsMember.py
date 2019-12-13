e4339f88f9f2095d43dc2d8ef3c6a49efaa353d2
Keith Walker <walkerk1980@gmail.com>
2019-12-12 18:36:51 -0800

    Fixed error with authorizing regions

--- ConfigCreateAggregatorInOrgsMember.py
+++ ConfigCreateAggregatorInOrgsMember.py
@@ -13,6 +13,7 @@ import boto3
 ORGS_ACCESS_ROLE_NAME = 'OrganizationAccountAccessRole'
 
 CONFIGURATION_AGGREGATOR_NAME = 'ConfigAggregator1'
+aggregator_region = None
 
 # disabled getting regions automatically for now as
 # get_available_regions() returns unsupported regions for Config Aggregator
@@ -20,8 +21,6 @@ CONFIGURATION_AGGREGATOR_NAME = 'ConfigAggregator1'
 regions = 'ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 \
 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2'
 config_regions = regions.split()
-
-continue_on_error = None
 
 orgs = boto3.client('organizations')
 
@@ -33,6 +32,25 @@ except Exception as e:
 
 master_account_id = organization['MasterAccountId']
 
+aggregation_accounts_with_errors = []
+
+continue_on_error = None
+def keep_going(account):
+        print('An error occoured in ' + account)
+        global continue_on_error
+        while continue_on_error not in ['y', 'a']:
+            continue_on_error = input('Do you want to continue? Y/A/N: ')
+            if continue_on_error.lower().startswith('y'):
+                print("Continuing")
+                continue_on_error = 'unknown'
+                return
+            elif continue_on_error.lower().startswith('a'):
+                print("Continuing")
+                continue_on_error = 'a'
+            elif continue_on_error.lower().startswith('n'):
+                print("Exiting")
+                exit(1)
+
 try:
     account_ids = []
     paginator = orgs.get_paginator('list_accounts')
@@ -41,7 +59,9 @@ try:
             account_ids.append(account['Id'])
             print(account['Id'])
     aggregator_account = input(
-        'Please choose the Account where you want the Aggregator to reside in: ')
+        'Please choose the Account that you want the Aggregator to created in: ')
+    aggregator_region = input(
+        'Please choose the Region that you want the Aggregator to be created in: ')
     if aggregator_account not in account_ids:
         print('The Account Id that you entered is not within the Organization!')
         exit(1)
@@ -51,6 +71,38 @@ try:
         exit(1)
 except Exception as e:
     print(e)
+
+def put_auth(config_client, account, aggregator_region, config_region):
+    try:
+        config_client.put_aggregation_authorization(
+            AuthorizedAccountId=account,
+            AuthorizedAwsRegion=aggregator_region
+        )
+        authorizations = master_config.describe_aggregation_authorizations()
+        print('Sucessfully authorized Aggregator in ' + config_region + ' in ' + account + ': ')
+        for authorization in authorizations.get('AggregationAuthorizations'):
+            if authorization.get('AuthorizedAwsRegion') == config_region:
+                print(authorization.get('AuthorizedAwsRegion'))
+                print('Success!')
+    except Exception as re:
+        print('Error accpeting in ' + config_region)
+        print(re)
+        return(re)
+
+def delete_auth(config_client, account, aggregator_region, config_region):
+    try:
+        config_client.delete_aggregation_authorization(
+            AuthorizedAccountId=account,
+            AuthorizedAwsRegion=aggregator_region
+        )
+        authorizations = master_config.describe_aggregation_authorizations()
+        print('Sucessfully Deleted authorization in Region ' + config_region + ' in ' + account + ': ')
+        for authorization in authorizations.get('AggregationAuthorizations'):
+            print('Deleting authorizations in ' + account + ' ' + config_region)
+    except Exception as re:
+        print('Error deleting in ' + config_region)
+        print(re)
+        pass
 
 sts = boto3.client('sts')
 
@@ -70,9 +122,8 @@ config = boto3.client('config',
                       aws_access_key_id=member_credentials['AccessKeyId'],
                       aws_secret_access_key=member_credentials['SecretAccessKey'],
                       aws_session_token=member_credentials['SessionToken'],
+                      region_name=aggregator_region
                       )
-
-aggregation_accounts_with_errors = []
 
 for account in account_ids:
     print('Accepting Authorizations in Account: ' + account)
@@ -84,49 +135,28 @@ for account in account_ids:
                 RoleArn=account_orgs_role_arn,
                 RoleSessionName='ConfigAggregatorScript',
             )['Credentials']
-            member_config = boto3.client('config',
-                                         aws_access_key_id=credentials['AccessKeyId'],
+            member_session = boto3.Session(aws_access_key_id=credentials['AccessKeyId'],
                                          aws_secret_access_key=credentials['SecretAccessKey'],
                                          aws_session_token=credentials['SessionToken'],
-                                         )
-            for region in config_regions:
-                print('Authorizing Region: ' + region)
-                member_config.put_aggregation_authorization(
-                    AuthorizedAccountId=aggregator_account,
-                    AuthorizedAwsRegion=region
                 )
-            authorizations = member_config.describe_aggregation_authorizations()
-            print('Sucessfully Authorized Regions in ' + account + ': ')
-            for authorization in authorizations['AggregationAuthorizations']:
-                print(authorization['AuthorizedAwsRegion'])
+            for config_region in config_regions:
+                member_config = member_session.client('config', region_name=config_region)
+                print('Authorizing Region: ' + config_region)
+                e = put_auth(member_config, aggregator_account, aggregator_region, config_region)
+                if e:
+                    raise e
         if account == master_account_id:
-            master_config = boto3.client('config')
-            for region in config_regions:
-                print('Authorizing Region: ' + region)
-                master_config.put_aggregation_authorization(
-                    AuthorizedAccountId=aggregator_account,
-                    AuthorizedAwsRegion=region
-                )
-            authorizations = master_config.describe_aggregation_authorizations()
-            print('Sucessfully Authorized Regions in ' + account + ': ')
-            for authorization in authorizations['AggregationAuthorizations']:
-                print(authorization['AuthorizedAwsRegion'])
+            master_session = boto3.Session()
+            for config_region in config_regions:
+                master_config = master_session.client('config', region_name=config_region)
+                print('Authorizing Region: ' + config_region)
+                e = put_auth(master_config, aggregator_account, aggregator_region, config_region)
+                if e:
+                    raise e
     except Exception as e:
         print(e)
-        print('An error occoured in ' + account)
+        keep_going(account)
         aggregation_accounts_with_errors.append(account)
-        while continue_on_error not in ['y', 'a']:
-            continue_on_error = input('Do you want to continue? Y/A/N: ')
-            if continue_on_error.lower().startswith('y'):
-                print("Continuing")
-                continue_on_error = 'unknown'
-                break
-            elif continue_on_error.lower().startswith('a'):
-                print("Continuing")
-                continue_on_error = 'a'
-            elif continue_on_error.lower().startswith('n'):
-                print("Exiting")
-                exit(1)
 
 try:
     config.put_configuration_aggregator(
